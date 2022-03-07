0023f29a02c1178feb7032eef158eb85476687bb
Navitasardar <navitasardar13@gmail.com>
2022-03-07 08:22:18 +0530

    adding project

---
+++ LockerCompany/src/lockercompanypvtlmt/Main1/LockerProject.java
@@ -0,0 +1,12 @@
+package lockercompanypvtlmt.Main1;
+
+public class LockerProject {
+	
+	
+		public static void main(String[] args) {
+			System.out.println("**********WELCOME TO LOCKER APPLICATION**********\n"
+					+ "----------Developer Name:- NAVITA SARDAR----------");
+			WelcomeScreen welcomeScreen = new WelcomeScreen(); // creating object to display welcome screen
+			welcomeScreen.display();
+		}
+	}
